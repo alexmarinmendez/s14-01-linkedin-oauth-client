02f7ce0e7e81f3c34d1e2c6112e43cddc388daeb
Alex Marin Mendez <alexmarinmendez@gmail.com>
2024-04-17 11:36:17 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import LinkedInOAuth from './components/LinkedInOAuth';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>LinkedIn OAuth Example</h1>
+        <LinkedInOAuth />
+      </header>
+    </div>
+  );
+}
+
+export default App;
